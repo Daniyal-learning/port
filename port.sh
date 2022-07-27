8cd682295912c6e3ce8d8fe1f54c4e343af8ecca
Daniyal-learning <78045149+Daniyal-learning@users.noreply.github.com>
2022-07-27 15:07:14 +0500

    Create port.sh
    This is for private servers only.

---
+++ port.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+text="ERROR"
+echo -e "Provide Port Number:"
+read port < /dev/tty
+
+re='^[0-9]+$'
+if ! [[ $port =~ $re ]] ; then
+   echo "ERROR: Not a Valid Port Number" >&2; exit 1
+fi
+
+sed -i "/tcp.*4200/i ACCEPT\t\tnet\tfw\ttcp\t$port\t\t\t#Custom port by PO" "/etc/shorewall/rules"
+
+echo "Opening Port..."
+
+shorewall update > /dev/null 2>&1
+out=$(shorewall restart 2>&1 | grep 'ERROR')
+
+if [[ "$out" == *"$text"* ]]; then
+  echo "There is something wrong! Please contact any senior."
+	exit
+fi
+echo -e "Port has been enabled."
